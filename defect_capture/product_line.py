565ad5d4310de9a183847f067127821f22720c1c
vibs94 <vibodha.14@cse.mrt.ac.lk>
2018-10-22 09:09:11 +0100

    Fixed segmentation bug, add light on and off API

--- defect_capture/product_line.py
+++ defect_capture/product_line.py
@@ -6,53 +6,54 @@ import datetime
 from PIL import Image
 from io import BytesIO
 
+class ProductLine:
+        #camera = ''
+        panel = ""
+        colorPredict = ""
+        
+        def __init__(self):
+                #ProductLine.camera = camera_dev
+                ProductLine.panel = LedPanel()
+                ProductLine.colorPredict = ColorPredict()
 
-camera = ""
-panel = ""
-colorPredict = ""
+        def setLEDColor(self,r,g,b):
+                colo = str(r)+" "+str(g)+" "+str(b)
+                ProductLine.panel.set_color_rgb(colo)
 
-class ProductLine:                
-	def __init__(self):
-		ProductLine.camera = Camera()
-		ProductLine.panel = LedPanel()
-		ProductLine.colorPredict = ColorPredict()
+        def getClothColor(self, camera_dev):
+                older_name = 'raw_cloth'
+                filename='cloth'
+                    
+                ProductLine.panel.set_color_rgb('255 50 100')
+                #ProductLine.camera.capture(folder_name,filename,0)
+                camera_dev.capture(folder_name,filename,0)
+                        
+                pc = PixelCounter(folder_name,filename)
+                r,g,b = pc.averagePixels()
 
-	def setLEDColor(self,r,g,b):
-		colo = str(r)+" "+str(g)+" "+str(b)
-		ProductLine.panel.set_color_rgb(colo)
+                colo = ProductLine.colorPredict.predict(r,g,b)
+                print("=============================================== Cloth color captured - "+colo+" ===============================================")
 
-	def getClothColor(self):
-		older_name = 'raw_cloth'
-		filename='cloth'
-		    
-		ProductLine.panel.set_color_rgb('255 50 100')
-		ProductLine.camera.capture(folder_name,filename,0)
-		        
-		pc = PixelCounter(folder_name,filename)
-		r,g,b = pc.averagePixels()
+                ProductLine.panel.set_color_rgb(colo)
+                return colo
 
-		colo = ProductLine.colorPredict.predict(r,g,b)
-		print("=============================================== Cloth color captured - "+colo+" ===============================================")
+        def getImage(self, camera_dev):
 
-		ProductLine.panel.set_color_rgb(colo)
-		return colo
-
-	def getImage(self):
-
-		time = datetime.datetime.now()
-		folder_name = 'new_cloth'
-		filename='cloth_'+ str(time)
-		
-		ProductLine.camera.capture(folder_name,filename,0)
-		        
-		print("================================================ Image captured ===============================================")
-		image = Image.open('/home/pi/Desktop/FYP/Images/'+folder_name+'/'+ filename+'.jpg')
-		x, y, h, w = 400, 150, 550, 500
-		croped_image = image.crop((x, y, x+h, y+w))
-##		croped_image.show()
-		buffered = BytesIO()
-		croped_image.save(buffered, format = "JPEG")
-		byte_image = buffered.getvalue()
-		return byte_image, folder_name, filename
-##		return "test"
+                time = datetime.datetime.now()
+                folder_name = 'new_cloth'
+                filename='cloth_'+ str(time)
+                
+                #ProductLine.camera.capture(folder_name,filename,0)
+                camera_dev.capture(folder_name,filename,0)
+                
+                print("================================================ Image captured ===============================================")
+                image = Image.open('/home/pi/Desktop/FYP/Images/'+folder_name+'/'+ filename+'.jpg')
+                x, y, h, w = 400, 150, 550, 500
+                croped_image = image.crop((x, y, x+h, y+w))
+##              croped_image.show()
+                buffered = BytesIO()
+                croped_image.save(buffered, format = "JPEG")
+                byte_image = buffered.getvalue()
+                return byte_image, folder_name, filename
+##              return "test"
 
--- defect_capture/rest_server.py
+++ defect_capture/rest_server.py
@@ -15,32 +15,42 @@ check = Check(0)
 productLine = ProductLine()
 
 class Start(Resource):
-	def get(self):		
-		if(check.isVal(0)):
-			check.reverseVal()
-			process.setup()
-			thread = threading.Thread(name='background', target=process.run)
-			thread.start()
-			print('********************************** thread started *********************************')
-			return {'status':'started'}
-		else:
-			return {'status':'already started'}
+        def get(self):          
+                if(check.isVal(0)):
+                        check.reverseVal()
+                        process.setup()
+                        thread = threading.Thread(name='background', target=process.run)
+                        thread.start()
+                        print('********************************** thread started *********************************')
+                        return {'status':'started'}
+                else:
+                        return {'status':'already started'}
 
 class Stop(Resource):
-	def get(self):
-		process.stop()
-		check.reverseVal()
-		return {'status':'stoped'}
+        def get(self):
+                process.stop()
+                check.reverseVal()
+                return {'status':'stoped'}
 
-##class OffLight(Resource):
-##	def get(self):
-##		productLine.setLEDColor(0,0,0)
-##		return {'status':'light off'}
+class OffLight(Resource):
+        def get(self):
+                productLine.setLEDColor(0,0,0)
+                return {'status':'light off'}
+
+class OnLight(Resource):
+        def get(self):
+                args = request.args
+                r = int(args['r'])
+                g = int(args['g'])
+                b = int(args['b'])
+                productLine.setLEDColor(r,g,b)
+                return {'status':'light off'}        
 
 
 api.add_resource(Start, '/start') # Route_1
 api.add_resource(Stop, '/stop') # Route_2
-api.add_resource(Stop, '/light_off') # Route_3
+api.add_resource(OffLight, '/light_off') # Route_3
+api.add_resource(OnLight, '/light_on') # Route_4
 
-if __name__ == '__main__':
-     app.run(port='5001')
+if __name__ == "__main__":
+     app.run(port='8000', debug=False)
