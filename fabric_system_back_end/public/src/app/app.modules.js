3d7f0572db61e2ebd6e3275e45119a85956ac309
Prasanna <prasannadeshappriya@gmail.com>
2018-11-07 23:29:07 +0530

    Improve performance of the python script handler

--- fabric_system_back_end/public/src/app/app.modules.js
+++ fabric_system_back_end/public/src/app/app.modules.js
@@ -5,5 +5,8 @@ let app = angular.module('fabric-system-front-end', [
 
 //Configure the constants
 app.constant(
-    'host_url', 'http://localhost:3000/admin/'
-);
+    'host_url', 'http://localhost:3000/admin/');
+
+//Spcket server 
+app.constant(
+    'socket_server_url', 'http://localhost:3000');
